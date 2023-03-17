83306547e8a33d214b6f6391145eaad784b73e6c
jonasdamasceno <jonasmichael2021@outlook.com>
2023-03-17 17:34:57 -0300

    backaup

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -9,27 +9,4 @@ serverRouter.use(express.json());
 serverRouter.get('/', async (_req, res) =>
   res.status(200).json(await loadTalkersData()));
 
-serverRouter.get('/id:', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const talker = await loadTalkersData(id);
-    return res.status(200).json(talker);
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(404)
-      .json({ message: 'Pessoa palestrante não encontrada' });
-  }
-});
-
-async function getTalkerById(id) {
-  const talkers = await loadTalkersData();
-  const talker = talkers.find((t) => t.id === +id);
-  if (!talker) {
-    throw new Error('Pessoa palestrante não encontrada');
-  }
-  return talker;
-}
-getTalkerById();
-
 module.exports = serverRouter;
