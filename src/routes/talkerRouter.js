dca62a592e657bf63a78ece4c945b1e3e766019f
jonasdamasceno <jonasmichael2021@outlook.com>
2023-03-20 15:02:36 -0300

    nova tentativa do requisito 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -9,4 +9,16 @@ serverRouter.use(express.json());
 serverRouter.get('/', async (_req, res) =>
   res.status(200).json(await loadTalkersData()));
 
+serverRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+    const talkers = await loadTalkersData();
+    const talker = talkers.find((t) => t.id === +id);
+    if (!talker) {
+      return res
+        .status(404)
+        .json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(talker);
+});
 module.exports = serverRouter;
