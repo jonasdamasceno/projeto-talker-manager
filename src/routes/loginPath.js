944ddf67162b791c54cb2decb4573bc0ecccba3d
jonasdamasceno <jonasmichael2021@outlook.com>
2023-03-21 13:47:19 -0300

    feat: implementação da função de validação de email e senha, requisito 4

--- src/routes/loginPath.js
+++ src/routes/loginPath.js
@@ -1,14 +1,37 @@
 const express = require('express');
-
 const randomToken = require('../utils/randomToken');
 
 const serverRouter = express.Router();
-
 serverRouter.use(express.json());
 
-serverRouter.post('/', (_req, res) => {
-    const token = randomToken();
-    return res.status(200).json({ token });
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const loginValidation = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!validateEmail(email)) {
+    return res
+      .status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res
+      .status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+serverRouter.post('/', loginValidation, (_req, res) => {
+  const token = randomToken();
+  return res.status(200).json({ token });
 });
 
 module.exports = serverRouter;
